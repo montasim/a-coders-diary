e08e811369cee799e0ebff61926a5d7be82c12bb
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-28 20:23:53 +0600

    create post page

--- src/Pages/Post/CreatePost.js
+++ src/Pages/Post/CreatePost.js
@@ -2,27 +2,26 @@ import React from 'react';
 
 const CreatePost = () => {
     return (
-        <div className='h-screen flex flex-col justify-center items-center lg:mx-8 sm:mx-4'>
-            <h3 className='text-2xl text-secondary my-12'>Create A Blog Post Here</h3>
+        <div className='flex flex-col h-screen mx-8'>
+            <h3 className='text-3xl text-secondary text-center mt-20'>Create A Blog Post Here</h3>
+            <form className='m-auto'>
+                <input type="text" placeholder="Post Title" class="input input-secondary w-full max-w-xl" />
 
-            <div className='grid'>
-                <form className='grid-cols-8 items-center justify-center'>
-                    <input type="text" placeholder="Post Title" class="input input-secondary w-full max-w-xl" />
+                <input type="text" placeholder="Post Image" class="input input-secondary w-full max-w-xl mt-4" />
 
-                    <textarea class="textarea textarea-secondary w-full max-w-xl my-4" placeholder="Post Description"></textarea>
+                <textarea class="textarea textarea-secondary w-full max-w-xl my-4" placeholder="Post Description"></textarea>
 
-                    <input type="text" placeholder="Post Tags" class="input input-secondary w-full max-w-xl" />
+                <input type="text" placeholder="Post Tags" class="input input-secondary w-full max-w-xl" />
 
-                    <select class="select select-secondary w-full max-w-xl my-4">
-                        <option>T-shirts</option>
-                        <option>Mugs</option>
-                    </select>
+                <select class="select select-secondary w-full max-w-xl my-4">
+                    <option>T-shirts</option>
+                    <option>Mugs</option>
+                </select>
 
-                    <br />
+                <br />
 
-                    <button type='submit' class="btn btn-secondary text-white btn-xs sm:btn-sm md:btn-lg lg:btn-md">Responsive</button>
-                </form>
-            </div>
+                <button type='submit' class="btn btn-secondary text-white btn-xs sm:btn-sm md:btn-lg lg:btn-md">Create A Post</button>
+            </form>
         </div>
     );
 };
