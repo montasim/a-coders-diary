1bc2189f92a2d148a11776e3421c22024896d9e1
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-31 10:49:42 +0600

    display loading screen & error for social login

--- src/Components/SocialLogin.js
+++ src/Components/SocialLogin.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import auth from '../Hooks/Firebase.Init';
+import { useSignInWithGoogle, useSignInWithFacebook, useSignInWithGithub, useSignInWithMicrosoft, useSignInWithTwitter, useSignInWithYahoo } from 'react-firebase-hooks/auth';
+import Loading from './Loading';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, facebookUser, facebookLoading, facebookError] = useSignInWithFacebook(auth);
+    const [signInWithGithub, githubUser, githubLoading, githubError] = useSignInWithGithub(auth);
+    const [signInWithMicrosoft, microsoftUser, microsoftLoading, microsoftError] = useSignInWithMicrosoft(auth);
+    const [signInWithTwitter, twitterUser, twitterLoading, twitterError] = useSignInWithTwitter(auth);
+    const [signInWithYahoo, yahooUser, yahooLoading, yahooError] = useSignInWithYahoo(auth);
+    let errorMessage = googleError || facebookError || githubError || microsoftError || twitterError || yahooError;
+
+    if (googleError || facebookError || githubError || microsoftError || twitterError || yahooError) {
+        errorMessage = <p className='text-center text-red-500 text-lg'>{errorMessage?.message?.slice(17, -2)}</p>
+    };
+
+    if (googleLoading || facebookLoading || githubLoading || microsoftLoading || twitterLoading || yahooLoading) {
+        return <Loading />;
+    };
+
     return (
         <div class="max-w-md mx-auto mt-8 space-y-4">
             <div class="divider">OR</div>
+
+            {
+                errorMessage
+            }
 
             <button
                 class="w-full inline-block px-5 py-3 ml-3 text-sm font-medium text-white bg-blue-500 rounded-lg"
