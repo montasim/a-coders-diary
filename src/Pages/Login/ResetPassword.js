38487d60938158a40644d96cad94b942b6b79c08
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-31 16:54:59 +0600

    password reset email

--- src/Pages/Login/ResetPassword.js
+++ src/Pages/Login/ResetPassword.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdOutlineAlternateEmail } from 'react-icons/md';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import auth from '../../Hooks/Firebase.Init';
+import Loading from '../../Components/Loading';
+import { toast } from 'react-toastify';
 
 const ResetPassword = () => {
+    const [email, setEmail] = useState('');
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+    let errorMessage = '';
+
+    if (error) {
+        errorMessage = <p className='text-center text-red-500 text-lg'>{error?.message?.slice(17, -2)}</p>
+    };
+
+    if (sending) {
+        return <Loading />;
+    };
+
+    const resetPassword = async (event) => {
+        event.preventDefault();
+
+        await sendPasswordResetEmail(email);
+
+        toast.success('Password reset email verification sent');
+
+        event.target.reset();
+    };
+
     return (
         <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
             <div className="max-w-lg mx-auto">
@@ -12,14 +38,14 @@ const ResetPassword = () => {
                     We will send you an email to reset your password.
                 </p>
 
-                <form action="" className="p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl">
+                <form onSubmit={resetPassword} className="p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl">
                     <p className="text-lg font-medium">Enter your email address</p>
 
                     <div>
                         <label for="email" className="text-sm font-medium">Email</label>
 
                         <div className="relative mt-1">
-                            <input
+                            <input onBlur={(e) => setEmail(e.target.value)}
                                 type="email"
                                 id="email"
                                 className="w-full p-4 pr-12 text-sm border-info rounded-lg shadow-sm"
@@ -31,6 +57,10 @@ const ResetPassword = () => {
                             </span>
                         </div>
                     </div>
+
+                    {
+                        errorMessage
+                    }
 
                     <button type="submit" className="block w-full px-5 py-3 text-sm font-medium text-white bg-gradient-to-r from-primary to-secondary hover:bg-primary rounded-lg">
                         Reset Password
