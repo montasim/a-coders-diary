ddee96fbd2817af320ec9aa5c46a7d726293479e
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-31 16:13:44 +0600

    gradient loading page

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className='flex h-screen items-center justify-center'>
-            <progress className="progress progress-primary w-56"></progress>
+            <progress className="progress progress-gradient-to-r from-primary to-secondary w-56 h-2 lg:h-3"></progress>
         </div>
     );
 };
