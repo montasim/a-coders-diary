0297b609a285ed7f76ca39f475d4109aa89271f6
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-02 20:26:52 +0600

    admins page

--- src/Pages/Dashboard/Admins/Admins.js
+++ src/Pages/Dashboard/Admins/Admins.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Admin from './Admin';
 
 const Admins = () => {
+    const [admins, setAdmins] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/posts')
+            .then(res => res.json())
+            .then(data => setAdmins(data));
+    }, [admins]);
+
     return (
         <div>
-            <h2>All Admins</h2>
+            <div class="overflow-x-auto w-full">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Serial</th>
+                            <th>Description</th>
+                            <th>Author</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            admins?.map((admin, index) => <Admin key={index} admin={admin} index={index} />)
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Serial</th>
+                            <th>Description</th>
+                            <th>Author</th>
+                            <th>Action</th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/Users/Users.js
+++ src/Pages/Dashboard/Users/Users.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import User from './User';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/posts')
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, [users]);
+
     return (
         <div>
-            <h2>All Users</h2>
+            <div class="overflow-x-auto w-full">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Serial</th>
+                            <th>Description</th>
+                            <th>Author</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users?.map((user, index) => <User key={index} user={user} index={index} />)
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Serial</th>
+                            <th>Description</th>
+                            <th>Author</th>
+                            <th>Action</th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
         </div>
     );
 };
