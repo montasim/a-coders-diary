8d250999db80981be9dae7fb5320216b67e5fd4f
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-02 20:33:43 +0600

    display user image in summary

--- src/Pages/Dashboard/DashboardSummary.js
+++ src/Pages/Dashboard/DashboardSummary.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth/dist/index.cjs';
+import { toast } from 'react-toastify';
+import Loading from '../../Components/Loading';
+import auth from '../../Hooks/Firebase.Init';
+import defaultUserImage from '../../Assets/Images/defaultUser.png';
 
 const DashboardSummary = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    if (loading) {
+        return <Loading />;
+    };
+
+    if (error) {
+        toast.error(`${error?.message?.slice(17, -2)}`);
+    };
+
     return (
         <div className='mx-auto my-8'>
             <div class="stats shadow">
@@ -9,7 +24,7 @@ const DashboardSummary = () => {
                     <div class="stat-figure text-primary">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                     </div>
-                    <div class="stat-title">Total Likes</div>
+                    <div class="stat-title">Total Posts</div>
                     <div class="stat-value text-primary">25.6K</div>
                     <div class="stat-desc">21% more than last month</div>
                 </div>
@@ -18,7 +33,7 @@ const DashboardSummary = () => {
                     <div class="stat-figure text-secondary">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                     </div>
-                    <div class="stat-title">Page Views</div>
+                    <div class="stat-title">Posts Views</div>
                     <div class="stat-value text-secondary">2.6M</div>
                     <div class="stat-desc">21% more than last month</div>
                 </div>
@@ -27,7 +42,7 @@ const DashboardSummary = () => {
                     <div class="stat-figure text-secondary">
                         <div class="avatar online">
                             <div class="w-16 rounded-full">
-                                <img src="https://api.lorem.space/image/face?w=128&h=128" />
+                                <img src={user?.photoURL || user?.user?.photoURL || defaultUserImage} />
                             </div>
                         </div>
                     </div>
