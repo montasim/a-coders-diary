0bb38e7ac0b1c89124d7c481937e068ba72c0366
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-03 20:12:58 +0600

    admin page

--- src/Pages/Dashboard/Admins/CreateAdmin.js
+++ src/Pages/Dashboard/Admins/CreateAdmin.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CreateAdmin = ({ createAdmin, index }) => {
-    const { _id, tagName, tagAuthor, tagDateTime } = createAdmin;
+    const { userName, userEmail, userImg, userCreationTime } = createAdmin;
 
     return (
         <tr>
@@ -16,28 +16,28 @@ const CreateAdmin = ({ createAdmin, index }) => {
                 <div className="flex items-center space-x-3">
                     <div className="avatar">
                         <div className="mask mask-squircle w-12 h-12">
-                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                            <img src={userImg} alt="Avatar Tailwind CSS Component" />
                         </div>
                     </div>
                     <div className='w-74'>
-                        <h3 className="font-bold">{tagName}</h3>
+                        <h3 className="font-bold">{userName}</h3>
                         <p className="text-sm opacity-50"></p>
-                        <span className="badge badge-ghost badge-sm">{tagDateTime}</span>
+                        <span className="badge badge-ghost badge-sm">{userCreationTime}</span>
                     </div>
                 </div>
             </td>
             <td>
                 <br />
-                <span className="text-xs">{tagAuthor}</span>
+                <span className="text-xs">{userEmail}</span>
             </td>
             <td>
                 <div className="flex items-center">
                     <button
-                        className="z-30 block p-2 text-red-700 transition-all bg-red-100 border-2 border-white rounded-full hover:scale-110 focus:outline-none focus:ring active:bg-red-50"
+                        class="z-10 block p-2 text-green-700 transition-all bg-green-100 border-2 border-white rounded-full active:bg-green-50 hover:scale-110 focus:outline-none focus:ring"
                         type="button"
                     >
-                        <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                         </svg>
                     </button>
                 </div>
--- src/Pages/Dashboard/Admins/CreateAdmins.js
+++ src/Pages/Dashboard/Admins/CreateAdmins.js
@@ -5,7 +5,7 @@ const CreateAdmins = () => {
     const [createAdmins, setCreateAdmins] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/posts')
+        fetch('http://localhost:5000/users')
             .then(res => res.json())
             .then(data => setCreateAdmins(data));
     }, [createAdmins]);
