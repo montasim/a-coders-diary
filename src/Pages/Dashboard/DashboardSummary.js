65c8d1c8047f8f3dc563bf73a67a877b2cfa133b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-03 20:51:58 +0600

    dashboard summary page

--- src/Pages/Dashboard/DashboardSummary.js
+++ src/Pages/Dashboard/DashboardSummary.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth/dist/index.cjs';
 import { toast } from 'react-toastify';
 import Loading from '../../Components/Loading';
@@ -7,6 +7,20 @@ import defaultUserImage from '../../Assets/Images/defaultUser.png';
 
 const DashboardSummary = () => {
     const [user, loading, error] = useAuthState(auth);
+    const [posts, setPosts] = useState([]);
+    const [tags, setTags] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/posts')
+            .then(res => res.json())
+            .then(data => setPosts(data));
+    }, [posts]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/tags')
+            .then(res => res.json())
+            .then(data => setTags(data));
+    }, [tags]);
 
     if (loading) {
         return <Loading />;
@@ -25,7 +39,7 @@ const DashboardSummary = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                     </div>
                     <div className="stat-title">Total Posts</div>
-                    <div className="stat-value text-primary">25.6K</div>
+                    <div className="stat-value text-primary">{posts?.length}</div>
                     <div className="stat-desc">21% more than last month</div>
                 </div>
 
@@ -33,8 +47,8 @@ const DashboardSummary = () => {
                     <div className="stat-figure text-secondary">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                     </div>
-                    <div className="stat-title">Posts Views</div>
-                    <div className="stat-value text-secondary">2.6M</div>
+                    <div className="stat-title">Total Tags</div>
+                    <div className="stat-value text-secondary">{tags?.length}</div>
                     <div className="stat-desc">21% more than last month</div>
                 </div>
 
