90c136dc624c0491c2b6b11670ea6a9dd5051ec9
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-05 00:03:02 +0600

    posts page redesigned

--- src/Pages/Dashboard/Posts/Posts.js
+++ src/Pages/Dashboard/Posts/Posts.js
@@ -18,21 +18,19 @@ const Posts = () => {
                         <tr>
                             <th>Serial</th>
                             <th>Description</th>
-                            <th>Author</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {posts?.length === 0 ?
                             <p class="text-center text-info text-xl my-20">Sorry! No post to display.</p>
-                            : posts?.map((post, index) => <Post key={index} post={post} />)
+                            : posts?.map((post, index) => <Post key={index} post={post} index={index} />)
                         }
                     </tbody>
                     <tfoot>
                         <tr>
                             <th>Serial</th>
                             <th>Description</th>
-                            <th>Author</th>
                             <th>Action</th>
                         </tr>
                     </tfoot>
