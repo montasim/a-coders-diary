36d880d4ac13776c98556db76213d4fbcd531daa
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-04 13:22:51 +0600

    blog post details page

--- src/Pages/BlogPostDetails/BlogPostDetails.js
+++ src/Pages/BlogPostDetails/BlogPostDetails.js
@@ -6,6 +6,7 @@ import rehypeRaw from 'rehype-raw';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import { remarkExtendedTable, extendedTableHandlers } from 'remark-extended-table';
+import { FiClock } from 'react-icons/fi';
 
 const BlogPostDetails = () => {
     const { _id } = useParams();
@@ -24,9 +25,17 @@ const BlogPostDetails = () => {
         <section className='mx-8'>
             <div className="font-sans mb-6">
                 <p className="text-base md:text-sm text-green-500 font-bold">&lt; <Link to="/" className="text-base md:text-sm text-green-500 font-bold no-underline hover:underline">BACK TO BLOG</Link></p>
-                <h1 className="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">{postName}</h1>
+                <h1 className="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">
+                    <ReactMarkdown
+                        rehypePlugins={[rehypeRaw]}
+                        remarkPlugins={[[remarkGfm, { singleTilde: false }], remarkGfm, remarkMath, rehypeKatex, remarkExtendedTable]}
+                        children={postName} />
+                </h1>
                 <p className="text-sm md:text-base font-normal text-gray-600">Category: {postCategory}</p>
-                <p className="text-xs md:text-base font-normal text-gray-600"><small>Published on {postDateTime?.slice(0, 10)}</small></p>
+                <p className="text-xs md:text-base font-normal text-gray-600 flex items-center">
+                    <FiClock className='mr-2' />
+                    <small>{postDateTime?.slice(0, 10)}</small>
+                </p>
             </div>
 
             <div>
@@ -36,7 +45,7 @@ const BlogPostDetails = () => {
                     children={postDescription} />
             </div>
 
-            <div className="text-base md:text-sm text-gray-500 px-4 py-6">
+            <div className="text-base md:text-sm text-gray-500 py-6">
                 Tags: {postTags} .
             </div>
 
@@ -45,11 +54,11 @@ const BlogPostDetails = () => {
             <div className="flex w-full items-center font-sans px-4 py-12">
                 <img className="w-10 h-10 rounded-full mr-4" src={postAuthorImg} alt="Avatar of Author" />
                 <div className="flex-1 px-2">
-                    <p className="text-base font-bold text-base md:text-xl leading-none mb-2">{postAuthor?.split('@')[0]}</p>
+                    <p className="font-bold text-sm leading-none mb-2">{postAuthor?.split('@')[0]}</p>
                     <p className="text-gray-600 text-xs md:text-base">Author Rank</p>
                 </div>
                 <div className="justify-end">
-                    <button className="bg-transparent border border-gray-500 hover:border-green-500 text-xs text-gray-500 hover:text-green-500 font-bold py-2 px-4 rounded-full">About Author</button>
+                    <button className="bg-transparent hover:bg-primary border border-primary text-xs text-primary hover:text-white font-bold py-2 px-4 rounded-full">About Author</button>
                 </div>
             </div>
 
@@ -58,11 +67,11 @@ const BlogPostDetails = () => {
             <div className="font-sans flex justify-between content-center px-4 pb-12">
                 <div className="text-left">
                     <span className="text-xs md:text-sm font-normal text-gray-600">&lt; Previous Post</span><br />
-                    <p><a href="#" className="break-normal text-base md:text-sm text-green-500 font-bold no-underline hover:underline">Blog title</a></p>
+                    <p><a href="#" className="break-normal text-base md:text-sm text-primary font-bold no-underline hover:underline">Blog title</a></p>
                 </div>
                 <div className="text-right">
                     <span className="text-xs md:text-sm font-normal text-gray-600">Next Post &gt;</span><br />
-                    <p><a href="#" className="break-normal text-base md:text-sm text-green-500 font-bold no-underline hover:underline">Blog title</a></p>
+                    <p><a href="#" className="break-normal text-base md:text-sm text-primary font-bold no-underline hover:underline">Blog title</a></p>
                 </div>
             </div>
         </section>
