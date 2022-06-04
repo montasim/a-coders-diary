4feb90f8aa2ba164735bc4c748d3d25033abf081
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-05 02:16:16 +0600

    user profile page

--- src/Pages/User/Profile.js
+++ src/Pages/User/Profile.js
@@ -13,10 +13,12 @@ const Profile = () => {
     const currentUserEmail = user?.email || user?.user?.email;
 
     useEffect(() => {
-        fetch(`https://a-coders-diary.herokuapp.com/users/${currentUserEmail}`)
+        fetch(`https://a-coders-diary.herokuapp.com/users?userEmail=${currentUserEmail}`)
             .then(res => res.json())
             .then(data => setUserData(data));
     }, [currentUserEmail]);
+
+    console.log(userData);
 
     if (loading) {
         return <Loading />;
@@ -27,17 +29,17 @@ const Profile = () => {
     };
 
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl m-20 p-6">
-            <figure><img src='' alt="Album" /></figure>
-            <div class="card-body">
+        <div className="card lg:card-side bg-base-100 shadow-xl m-20 p-6">
+            <figure><img src={userData?.[0]?.userImg} alt="Album" /></figure>
+            <div className="card-body">
                 <div className='flex justify-between'>
                     <div>
-                        <h2 class="card-title">Author Name</h2>
-                        <p>Author Rank</p>
+                        <h2 className="card-title">{userData?.[0]?.userName}</h2>
+                        <p className='text-sm'>Member since {userData?.[0]?.userCreationTime?.slice(0, 10)}</p>
                     </div>
 
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-sm bg-gradient-to-r from-primary to-secondary text-white text-sm border-0">Edit Profile</button>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-sm bg-gradient-to-r from-primary to-secondary text-white text-sm border-0">Edit Profile</button>
                     </div>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, quidem error quisquam doloribus, hic exercitationem similique necessitatibus placeat mollitia harum recusandae. Fuga ipsam eum doloribus reprehenderit ipsum reiciendis obcaecati natus!</p>
