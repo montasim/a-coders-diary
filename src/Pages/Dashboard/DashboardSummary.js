5e6e8697b9c9c822d317174e5886ec834a6d2ba5
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-04 13:01:00 +0600

    dashboard summary text color

--- src/Pages/Dashboard/DashboardSummary.js
+++ src/Pages/Dashboard/DashboardSummary.js
@@ -44,25 +44,25 @@ const DashboardSummary = () => {
                 </div>
 
                 <div className="stat">
-                    <div className="stat-figure text-secondary">
+                    <div className="stat-figure text-primary">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                     </div>
                     <div className="stat-title">Total Tags</div>
-                    <div className="stat-value text-secondary">{tags?.length}</div>
+                    <div className="stat-value text-primary">{tags?.length}</div>
                     <div className="stat-desc">21% more than last month</div>
                 </div>
 
                 <div className="stat">
-                    <div className="stat-figure text-secondary">
+                    <div className="stat-figure text-primary">
                         <div className="avatar online">
                             <div className="w-16 rounded-full">
-                                <img src={user?.photoURL || user?.user?.photoURL || defaultUserImage} />
+                                <img src={user?.photoURL || user?.user?.photoURL || defaultUserImage} alt='user profile pic' />
                             </div>
                         </div>
                     </div>
                     <div className="stat-value">86%</div>
                     <div className="stat-title">Tasks done</div>
-                    <div className="stat-desc text-secondary">31 tasks remaining</div>
+                    <div className="stat-desc text-primary">31 tasks remaining</div>
                 </div>
 
             </div>
