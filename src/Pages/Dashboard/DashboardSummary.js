c059d18093cd2fe24814e00eb46b52b08466b7c6
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-04 19:26:19 +0600

    display total post of user in dashboard

--- src/Pages/Dashboard/DashboardSummary.js
+++ src/Pages/Dashboard/DashboardSummary.js
@@ -7,14 +7,18 @@ import defaultUserImage from '../../Assets/Images/defaultUser.png';
 
 const DashboardSummary = () => {
     const [user, loading, error] = useAuthState(auth);
-    const [posts, setPosts] = useState([]);
+    const [myPosts, setMyPosts] = useState([]);
     const [tags, setTags] = useState([]);
 
     useEffect(() => {
-        fetch('https://a-coders-diary.herokuapp.com/posts')
-            .then(res => res.json())
-            .then(data => setPosts(data));
-    }, [posts]);
+        if (user) {
+            fetch(`http://localhost:5000/my-posts?postAuthor=${user?.email || user?.user?.email}`, {
+                method: 'GET'
+            })
+                .then(res => res.json())
+                .then(data => setMyPosts(data));
+        }
+    }, [user]);
 
     useEffect(() => {
         fetch('https://a-coders-diary.herokuapp.com/tags')
@@ -39,17 +43,15 @@ const DashboardSummary = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                     </div>
                     <div className="stat-title">Total Posts</div>
-                    <div className="stat-value text-primary">{posts?.length}</div>
-                    <div className="stat-desc">21% more than last month</div>
+                    <div className="stat-value text-primary">{myPosts?.length}</div>
                 </div>
 
                 <div className="stat">
                     <div className="stat-figure text-primary">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                     </div>
-                    <div className="stat-title">Total Tags</div>
+                    <div className="stat-title">Total Category</div>
                     <div className="stat-value text-primary">{tags?.length}</div>
-                    <div className="stat-desc">21% more than last month</div>
                 </div>
 
                 <div className="stat">
@@ -62,7 +64,6 @@ const DashboardSummary = () => {
                     </div>
                     <div className="stat-value">86%</div>
                     <div className="stat-title">Tasks done</div>
-                    <div className="stat-desc text-primary">31 tasks remaining</div>
                 </div>
 
             </div>
